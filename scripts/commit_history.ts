02cbc6ec7d831db657f86c27f65871e5d3e8fa47
k-kuroguro <lingjiujinshan@gmail.com>
2023-04-09 17:07:06 +0900

    fix: Escape double quote in commit history

--- scripts/commit_history.ts
+++ scripts/commit_history.ts
@@ -3,13 +3,14 @@ import { basename, dirname, join, sep } from 'https://deno.land/std@0.179.0/path
 import { sha256 } from 'https://denopkg.com/chiefbiiko/sha256@v1.0.0/mod.ts';
 import { equal } from 'https://deno.land/x/equal@v1.5.0/mod.ts';
 
-type Commit = {
-   'id': string;
-   'subject': string;
-   'date': string;
-   'github_url': string;
+const Keys = ['id', 'subject', 'date', 'github_url'] as const;
+type Commit<T> = {
+   'id': T;
+   'subject': T;
+   'date': T;
+   'github_url': T;
 };
-type History = Commit[];
+type History = Commit<string>[];
 type HistoryDict = Record<string, History>;
 
 const getPostPaths = (posts_dir: string): string[] =>
@@ -41,37 +42,67 @@ const toNames = (paths: string[]): string[] =>
 const toGitHubUrl = (path: string): string =>
    `https://github.com/k-kuroguro/blog/commit/%H#diff-${sha256(path.replaceAll(sep, '/'), 'utf8', 'hex')}`;
 
-const runGitLog = async (paths: string[]): Promise<{ success: boolean; output: Uint8Array }[]> => {
-   const processes = paths.map((path) =>
-      Deno.run({
+const runGitLog = async (
+   paths: string[],
+): Promise<({ success: true; output: Commit<Uint8Array> } | { success: false; output: undefined })[]> => {
+   const run = (path: string, key: keyof Commit<string>): Deno.Process => {
+      const formats: Commit<string> = { 'id': '%H', 'subject': '%s', 'date': '%aI', 'github_url': toGitHubUrl(path) };
+      return Deno.run({
          cmd: [
             'git',
             'log',
-            `--pretty=format:{\
-               "id":"%H", \
-               "subject":"%s", \
-               "date":"%aI", \
-               "github_url": "${toGitHubUrl(path)}"\
-            },`,
+            // Use <double_quote> to simplify escaping of double quotes.
+            `--pretty=format: <double_quote>${formats[key as keyof Commit<string>]}<double_quote>,`,
             '--',
             path,
          ],
          stdout: 'piped',
          stderr: 'piped',
-      })
-   );
-   const statuses = await Promise.all(processes.map((p) => p.status()));
-   return Promise.all([...Array(statuses.length)].map(async (_, i) => ({
-      'success': statuses[i].success,
-      'output': await processes[i].output(),
-   })));
+      });
+   };
+
+   return await Promise.all(paths.map(async (path) => {
+      const processes = Keys.map((k) => run(path, k));
+      const success = (
+         await Promise.all(processes.map((p) => p.status()))
+      ).every((status) => status.success);
+      if (success) {
+         return {
+            success,
+            output: {
+               'id': await processes[0].output(),
+               'subject': await processes[1].output(),
+               'date': await processes[2].output(),
+               'github_url': await processes[3].output(),
+            },
+         };
+      } else {
+         return {
+            success,
+            output: undefined,
+         };
+      }
+   }));
 };
 
+// Escape double quotes except the first and last.
+const escape = (str: string): string => str.replaceAll('"', '\\"').replaceAll('<double_quote>', '"');
+
 const toHistory = (
-   raw_output: Uint8Array,
+   raw_output: Commit<Uint8Array>,
 ): History => {
-   const jsonString = '[' + new TextDecoder().decode(raw_output).slice(0, -1) + ']';
-   const history: History = JSON.parse(jsonString);
+   const json_outputs: string[][] = Keys.map((k) =>
+      JSON.parse(escape('[' + new TextDecoder().decode(raw_output[k]).slice(0, -1) + ']'))
+   );
+   const history: History = [];
+   for (let i = 0; i < json_outputs[0].length; i++) {
+      history.push({
+         'id': json_outputs[0][i],
+         'subject': json_outputs[1][i],
+         'date': json_outputs[2][i],
+         'github_url': json_outputs[3][i],
+      });
+   }
    return history.filter((commit) => commit.subject.startsWith('docs:')).map((commit) => ({
       ...commit,
       subject: commit.subject.replace(/^docs: /, ''),
@@ -116,8 +147,8 @@ const main = async () => {
    const post_paths = getPostPaths(posts_dir);
    const post_names = toNames(post_paths);
    const results = await runGitLog(post_paths);
-   const histories = results
-      .filter((result) => result.success)
+   const histories = (results
+      .filter((result) => result.success) as { success: true; output: Commit<Uint8Array> }[])
       .map((result) => toHistory(result.output));
    const json = toHistoryDict(post_names, histories);
 
